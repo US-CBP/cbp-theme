f18ec2bc1d2716a6c6dd769a768f45433f724c8c
David Hodge <david.hodge@lucidtechnics.com>
2016-09-13 18:13:01 +0000

    rebuild pages at 2b69dd8

--- app/js/app_tags.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	'use strict';
-
-	$('[data-dismiss="tag"]').each(function(idx, el) {
-		$(el).on('click', function(ev) {
-			$(ev.currentTarget.parentElement).remove();
-		});
-	});
-}($));
