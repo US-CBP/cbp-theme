fb23649f0e550417692493425fec6804054764ac
Menka Kayan <mkayan@users.noreply.github.com>
2017-10-03 12:13:51 -0400

    Issue 150: scrolling issues (#156)
    * Issue #42: accessibility fix on notification, fixes: extend alert scss and add aria to alerts and notification
    
    * merging theme-42 branch
    
    * merging branch to master
    
    * issue #07: header content moved from css to markup for better accessibility
    
    * Issue-112: adding sr only information for the alerts and notification
    
    * updating font size for input fields and fixing inconsistent font sizes
    
    * cleaning up styles
    
    * cleaning up styles
    
    * fixing someclass
    
    * fixing static label issue
    
    * changing the heading variables to fixed font size
    
    * adding resize for app header 0ffset
    
    * fixing the scrolling issue
    
    * upsting for layout with out app header
    
    * adding updates
    
    * fixing scrollin gissues
    
    * adding comments
    
    * adding comments
    
    * adding comments

--- app/js/app_navigation.js
+++ app/js/app_navigation.js
@@ -4,9 +4,9 @@ import $ from 'jquery';
 
   'use strict';
 
-  var lastPosition = 0;
+  // var lastPosition = 0;
 
-  $('.cbp-header[role="navigation"]').each(function() {
+  /*$('.cbp-header[role="navigation"]').each(function() {
     $(window).on('scroll', function() {
       var currentPosition = $(this).scrollTop();
 
@@ -38,12 +38,68 @@ import $ from 'jquery';
 
       lastPosition = currentPosition;
     });
-  });
+  });*/
+
+  
 
   $( document ).ready(function() {
-    $(window).resize(function() {
-        $('.content').css("padding-top", $(".app-header").height());
+    //make the header sticky
+    $(window).scrollTop(0);
+    $(window).resize(function() { 
+    
+    //height of only top header
+    var sidebarOffsetTopHeader = $(".cbp-header").height();
+
+    //height of only app header
+    var sidebarOffsetSingleHeader = $(".app-header").height();
+
+    //height of both header
+    var sidebarOffsetDoubleHeader = sidebarOffsetTopHeader + sidebarOffsetSingleHeader;
+
+    //dynamically add the offset to sidebar on page load
+    if($('.app-header').length === 0) {
+        $('.sidebar').css('top', sidebarOffsetTopHeader) ;
+      }
+    else {
+        $('.sidebar').css('top', sidebarOffsetDoubleHeader) ;
+    }
+
+
+    //scroll function 
+    $(window).scroll(function(){  
+      
+      //check when the scroll is more than the height of universal header
+      var scroll = $(window).scrollTop();
+
+      //check if the app header is there or not
+      if($('.app-header').length === 0) {
+        //check when the scroll is more than the height of universal header
+        if (scroll >= sidebarOffsetTopHeader) {
+          $('.sidebar').css('top', 0) ;
+        } 
+        else {
+          $('.sidebar').css('top', sidebarOffsetTopHeader);
+        }
+      } else {
+        //check when the scroll is more than the height of universal header
+        if (scroll >= sidebarOffsetTopHeader) {
+          $('.app-header').addClass('top');
+          $('.sidebar').css('top', sidebarOffsetSingleHeader) }
+        else {
+          $('.app-header').removeClass('top');
+          $('.sidebar').css('top', sidebarOffsetDoubleHeader) ;
+        }
+      }
+
+    
+    });
+    $('.content').css("padding-top", sidebarOffsetSingleHeader); 
+
+       
     }).resize();
+  
   });
+
+ 
 
 }($));
