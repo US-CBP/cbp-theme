d832e96494a4c706c8fca3beb90c106d67c922fa
Menka Kayan <menka.kayan@cbp.dhs.gov>
2017-10-04 16:16:22 -0400

    adding alternate color

--- app/js/app_navigation.js
+++ app/js/app_navigation.js
@@ -4,45 +4,7 @@ import $ from 'jquery';
 
   'use strict';
 
-  // var lastPosition = 0;
-
-  /*$('.cbp-header[role="navigation"]').each(function() {
-    $(window).on('scroll', function() {
-      var currentPosition = $(this).scrollTop();
-
-      if (!lastPosition) {
-        lastPosition = currentPosition;
-        return;
-      }
-
-      if (lastPosition > currentPosition) {
-        $('.cbp-header').removeClass('disappear');
-
-        //if not using app-header
-        if($('.app-header').length) {
-          $('.app-header').removeClass('top');
-          $('.sidebar').removeClass('after-header').addClass('after-double-header');
-        }
-      } else {
-        $('.cbp-header').addClass('disappear');
-        // hide all menu items from the header
-        $('.cbp-header .dropdown.open').removeClass('open');
-
-        if($('.app-header').length) {
-          $('.app-header').addClass('top');
-          $('.sidebar').removeClass('after-double-header').addClass('after-header');
-          $('.app-header .dropdown.open').removeClass('open');
-        }
-
-      }
-
-      lastPosition = currentPosition;
-    });
-  });*/
-
-  
-
-  $(window).load(function() {
+  $(window).on('load', function() {
     //make the header sticky
     $(window).scrollTop(0);
     $(window).resize(function() { 
