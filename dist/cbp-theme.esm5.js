70cb12c0b893edf9288a56270b815cbecda9c4ed
David Hodge <david.hodge@lucidtechnics.com>
2018-04-21 13:28:45 +0000

    rebuild pages at 8c6663f

--- dist/cbp-theme.esm5.js
+++ dist/cbp-theme.esm5.js
@@ -93,7 +93,7 @@ function applyThirdPartySelect() {
 
   var getDirty = function getDirty(idx, el) {
     if (el !== undefined && $(el).children('select').length > 0) {
-      $($(el).children('select').get(0)).change(function (ev) {
+      $($(el).children('select').get(0)).on("change", function (ev) {
         var option = $(ev.currentTarget).find('option:selected').val();
         var action = option && option.length > 0 ? 'addClass' : 'removeClass';
         $(el)[action]('is-dirty');
