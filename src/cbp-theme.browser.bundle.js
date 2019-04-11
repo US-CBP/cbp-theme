9f5455fb2838e27a86cc5848c2ae72e3c0bef55b
John Higgins <john.d.higgins@associates.cbp.dhs.gov>
2019-04-11 15:09:43 -0400

    upgraded to babel 7

--- src/cbp-theme.browser.bundle.js
+++ src/cbp-theme.browser.bundle.js
@@ -2,17 +2,26 @@
  * This is for using in scripts tag in browser.
  * cbpTheme object is populated in the browser.
  */
+// import 'babel-polyfill'
 
+
+ // because ES6 import syntax is being used at entry point, ensure the polyfills are loaded first
 import 'bootstrap'
 import 'material-design-lite'
 import 'mdl-selectfield/dist/mdl-selectfield'
 
 import {CBPTheme} from './cbp-theme'
+// babel-polyfill inclusion check
 
-if (!window._babelPolyfill) {
-  /* eslint-disable global-require */
-  require("babel-polyfill");
-}
+// if (!window._babelPolyfill) {
+//    /* eslint-disable global-require */
+//    require("babel-polyfill");
+//  }
+
+
+
+
+
 
 (function () {
   window.CBPTheme = CBPTheme
