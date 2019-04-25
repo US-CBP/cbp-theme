61b974aa520e3e5135b8e4ea249f94a312011664
John D. Higgins <HIGGO36@users.noreply.github.com>
2019-04-25 09:33:08 -0400

    Issue75 (#270)
    * issue#75 added necessary accessiblity styling for the input file type elements
    
    * added input file type element
    
    * fixed a bug in firefox
    
    * updates from David's comments
    
    * updates for adding new script to the kitchensink.js
    
    * removed unnecessary svg code
    
    * made updates to match style guide and removed unnecessary code
    
    * updated background styling to match theme's vendor scss
    
    * updates styling to include scss variables
    
    * added another file upload component to match current style guide's
    
    * moved css from index to forms.scss
    
    * just clarified some commments
    
    * updated indentation to follow specs
    
    * fixed some syntax issues
    
    * made syntax updates to match lint standards
    
    * made updates to reference existing variables where able

--- kitchensink/kitchensink.js
+++ kitchensink/kitchensink.js
@@ -131,3 +131,49 @@ $(document).ready(function () {
     hulkIt('top-secret')
   })
 })
+
+// Form Input File Type #1
+// Enable the <input type="file"> functionality
+/* By Osvaldas Valutis, www.osvaldas.info / Available for use under the MIT License
+*/
+
+;(function ($, _window, _document) {
+       $('.inputfile').each(function () {
+         var $input = $(this)
+         var $label = $input.next('label')
+         var labelVal = $label.html()
+
+     // changes the label text to show the name of the selected file
+         $input.on('change', function (e) {
+           var fileName = ''
+
+           if (this.files && this.files.length > 1) { fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length) } else if (e.target.value) { fileName = e.target.value.split('\\').pop() }
+
+           if (fileName) { $label.find('span').html(fileName) } else { $label.html(labelVal) }
+         })
+
+      // Firefox bug fix
+      // Applies and removes necessary accessiblity styling for focus events
+         $input
+         .on('focus', function () { $input.addClass('has-focus') })
+         .on('blur', function () { $input.removeClass('has-focus') })
+       })
+     })($, window, document)
+
+     // Form Input File Type #2
+     // trigger upload on space & enter
+     // = standard button functionality
+$('#buttonlabel span[role=button]').bind('keypress keyup', function (e) {
+  if (e.which === 32 || e.which === 13) {
+    e.preventDefault()
+    $('#fileupload').click()
+  }
+})
+
+     // return chosen filename to additional input
+$('#fileupload').change(function (e) {
+  var filename = $('#fileupload').val().split('\\').pop()
+  $('#filename').val(filename)
+  $('#filename').attr('placeholder', filename)
+  $('#filename').focus()
+})
