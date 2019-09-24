073e9dac20bdbc30464d9ff7e325c72ae43e3442
John Higgins <john.d.higgins@associates.cbp.dhs.gov>
2019-09-24 12:06:32 -0400

    updates for css framework build

---
+++ ds-css/src/js/index.js
@@ -0,0 +1,3 @@
+// SASS Files to compile/build and place in the dist/css/[name]bundle.css file
+// the [name] of the bundle.css file is derived from the file name from /styles/[name].scss file
+require('../styles/main.scss');
