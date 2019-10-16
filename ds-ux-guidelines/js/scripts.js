df494c133d2f8677b9aeab9c385d1d54cbdbab31
Ray Robertson <ray760@yahoo.com>
2019-10-16 10:05:03 -0400

    CBP DS initial site skeleton.  (#291)
    * Created new gitignore file
    
    * Set up inital html and css
    
    * Added basic top header
    
    * Added side menu navigation. need to update HTML.
    
    * Finished left navbar. Can't scroll to last item.
    
    * Side menu  now scrolls correctly. Added sidenav animation.
    
    * Navbar css and js added to show/hide on click.
    
    * Made updates to class names and structure in HTML and CSS
    
    * Added new class for active submenu.
    
    * Added some content to HTML for DS skeleton.
    
    * Minor html text\css updates.
    
    * Completed top nav layou, and html\css updates.
    
    * Updated .gitignore to iginore folder I can't delete
    
    * Linked to updated style guide, added in new class names and updated css styles.

---
+++ ds-ux-guidelines/js/scripts.js
@@ -0,0 +1,19 @@
+ const hamburger = document.querySelector('#main-content');
+ const navBar = document.querySelector('.cbp-side-nav');
+ const mainGrid = document.querySelector('.cbp-ds-grid');
+
+ /* Hamburger menu open/close code START */
+ 
+ hamburger.addEventListener( 'click', function(e) {
+   
+  if(navBar.classList.contains('cbp-side-nav--hide')) {
+    navBar.classList.remove('cbp-side-nav--hide');
+    mainGrid.classList.add('cbp-ds-grid--margin-left');
+  } else {
+    navBar.classList.add('cbp-side-nav--hide');
+    mainGrid.classList.remove('cbp-ds-grid--margin-left');
+  }
+
+ });
+
+ /* Hamburger menu open/close code START */
