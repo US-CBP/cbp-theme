8f0ed18c7773551bfd093c887cee87aa9ca9ba5e
John Higgins <john.d.higgins@associates.cbp.dhs.gov>
2019-10-28 11:53:12 -0400

    updates for extending our overall webpack build process to include the ds-guide directory and all new tools over the last week

--- ds-css/src/js/index.js
+++ ds-css/src/js/index.js
@@ -1,2 +1,10 @@
-
+// this file and all files or folders referenced here are actively watched for changes
 require('../styles/cbp-ds.scss');
+
+require('../../../ds-ux-guidelines/sass/styles.scss');
+
+require('../../../ds-ux-guidelines/js/scripts');
+
+
+
+
--- ds-css/webpack.config.js
+++ ds-css/webpack.config.js
@@ -1,4 +1,3 @@
-
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack'); // to access built-in plugins
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,69 +6,60 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-// const isDevelopment = process.env.NODE_ENV === 'development';
-
 module.exports = {
-  devtool: 'source-map',
-   // this creates the /dis/js/cbp-ds-bundle.js.map file
-  entry: './src/js/index.js',
+  devtool: 'source-map', // creates /dis/js/cbp-ds-bundle.js.map
+  entry: './src/js/index.js', // entry module that injects dependent files
   // controls the devServer process and localhost port
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    index: 'index.html',
-    watchOptions: {
-      poll: true
-      // this controls active watch mode for file changes
+  // devServer is short for 'webpack-dev-server'
+  devServer: {      
+    contentBase: path.join(__dirname, 'dist'), // where to serve content from
+    watchContentBase: true, // dev-server watches for changes to served files
+    index: 'index.html', // considered the the index file
+    watchOptions: { // provides customizations for watch mode
+  // this controls active watch mode for file changes w/in the directory containing the main source index.html file
+    ignored: '/node_modules/',
+      poll: true // creates a continous watch mode for devServer
+      // anything imported into the src/js/index.js file is watched
     },
-    watchContentBase: true,
-    compress: true,
-    port: 9000,
-    open: true,
-    overlay: {
-      warnings: true,
-      errors: true
+    compress: true, // compresses the HTTP requested files to .zip format
+    port: 9000, // local hosting port assigned
+    open: true, // auto open page
+    overlay: { // creates dark overlay for high-contrast display
+      warnings: true, // show any warnings
+      errors: true // show any errors
     }
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/cbp-ds-bundle.js'
+    path: path.resolve(__dirname, 'dist'), // location for output files
+    filename: 'js/cbp-ds-bundle.js' // name of compiled js build file
   },
-
-
-  // watch: true,
-
-
+  // watch: true, // (watches changes in any of the resolved files)
   module: {
     rules: [
       {
-      // below test covers both sass and css file types
-      test: /\.(sa|sc|c)ss$/,
-      use: [
-          
+      test: /\.(sa|sc|c)ss$/, // test covers both sass and css file types
+      use: [ 
           MiniCssExtractPlugin.loader,
-          
           {
-            loader: 'css-loader',
-            // options: {
-            //   sourceMap: true
-            // }
+            loader: 'css-loader', // interprets @import and url() like import/require() and will resolve them
           },
           {
-            loader: 'resolve-url-loader',
-            // provides the "url rewriting" that Sass is missing.
-            options: { // options...
-            }
+            loader: 'resolve-url-loader', // provides the "url rewriting" that Sass is missing.
           },
           {
-            loader: 'sass-loader',
+            loader: 'sass-loader', // Loads a Sass/SCSS file and compiles it to CSS
             options: {
-              sourceMap: true
+              sourceMap: true,
+              implementation: require('sass'), // Prefer `dart-sass`
+              sassOptions: {
+                // fiber: false,
+                // using fiber support by default, allowing jumps between
+                // call stacks from a single thread if needed.
+              },
             }
           },
-
         ]
     },
-   
     {
       test: /\.m?js$/,
       exclude: /(node_modules|bower_components)/,
@@ -89,31 +79,37 @@ module.exports = {
     new webpack.ProgressPlugin(), // show build progress details
     new CleanWebpackPlugin(),  // keeps track of all files in dist/
 
+    // an HTML template is required to serve the bundle
     new HtmlWebpackPlugin({
-      entry: './src/index.html',
+      entry: '../ds-ux-guidelines/index.html',
       hash: false, // set to 'true' if needing unique ID's at end of file name
-      title: 'CBP-DS Theme Website',
-      template: './src/index.html', // this will auto parse template as well
+      title: 'Style Guide - CBP Style Guide',
+      template: '../ds-ux-guidelines/index.html',
       inject: true,
       minify: {
           removeComments: false,
           collapseWhitespace: false
       }
   }),
-    new MiniCssExtractPlugin({
-      ignoreOrder: false, //enable to remove warnings about any possible conflict
+    new MiniCssExtractPlugin({ // extracts CSS into separate files per JS file that contains CSS
+      ignoreOrder: false, // enable to remove warnings about any possible conflict
       filename: 'css/cbp-ds.min.css'
-      // chunkFilename: 'css/cbp-ds.min.css.map'
     }),
 
-    new OptimizeCssAssetsPlugin({
+    new OptimizeCssAssetsPlugin({ // optimizes/minifies all CSS assets
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano'),
       cssProcessorPluginOptions: {
         preset: ['default', { 
           
           discardComments: { 
-          removeAll: true } 
+          removeAll: true },
+
+          cssDeclarationSorter: {
+            order: 'alphabetically' // formats properties in alphabetical order
+          },
+          normalizeWhitespace: true
+
         }],
       },
       canPrint: true
@@ -123,7 +119,9 @@ module.exports = {
 
   "options": {
     config: '.stylelintrc',
-    files: './src/styles/custom/**/*.scss',
+    files: '**/*.s?(a|c)ss',
+    // files: './src/styles/custom/**/*.scss',
+    // files: ['./src/styles/custom/**/*.scss', '../ds-ux-guidelines/sass/styles.scss'],
     formatter: 'string',
     cache: true, // only changed files get linted
     cacheLocation: './tests/',
@@ -136,6 +134,5 @@ module.exports = {
   }
 
   })
-
 ]
 }
