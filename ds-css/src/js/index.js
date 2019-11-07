733a4a2fbf7f624724fcd70d1f9e86dbf3f5f9c0
John Higgins <john.d.higgins@associates.cbp.dhs.gov>
2019-11-07 13:57:35 -0500

    made update to fix icons page issue and removed unncessary comments and mapped over file location changes to the pertaining scripts

--- ds-css/src/js/index.js
+++ ds-css/src/js/index.js
@@ -1,10 +1,6 @@
 // this file and all files or folders referenced here are actively watched for changes
 require('../styles/cbp-ds.scss');
 
-require('../../../ds-ux-guidelines/sass/styles.scss');
-
-require('../../../ds-ux-guidelines/js/scripts');
-
 
 
 
--- ds-css/webpack.config.js
+++ ds-css/webpack.config.js
@@ -1,20 +1,22 @@
+const path = require('path'); // this node module provides utilities for working with file and directory paths.
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack'); // to access built-in plugins
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
-// const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map', // creates /dis/js/cbp-ds-bundle.js.map
   entry: './src/js/index.js', // entry module that injects dependent files
   // controls the devServer process and localhost port
   // devServer is short for 'webpack-dev-server'
-  devServer: { // note: when running this, it will initially hide the /div/ folder by default, after which you can just run 'npm run build-webpack' to see the /dist/ folder locally   
-    contentBase: path.join(__dirname, 'dist'), // where to serve content from
+  devServer: { // note: when running this, it will initially hide the /div/ folder by default, after which you can just run 'npm run build-webpack' to see the /dist/ folder locally
+    // liveReload: true,   
+    contentBase: path.join(__dirname, '../ds-ux-guidelines/'), // what directory to serve content from
     watchContentBase: true, // dev-server watches for changes to served files
-    index: 'index.html', // considered the the index file
+    index: 'index.html', // acting as index from the 'contentBase' directory
     watchOptions: { // provides customizations for watch mode
   // this controls active watch mode for file changes w/in the directory containing the main source index.html file
     ignored: '/node_modules/',
@@ -23,23 +25,24 @@ module.exports = {
     },
     compress: true, // compresses the HTTP requested files to .zip format
     port: 9000, // local hosting port assigned
-    open: true, // auto open page
+    openPage: 'index.html', // this is default setting
+    open: true, // opens browser once server has started
     overlay: { // creates dark overlay for high-contrast display
       warnings: true, // show any warnings
       errors: true // show any errors
     }
   },
   output: {
-    path: path.resolve(__dirname, 'dist'), // location for output files
-    filename: 'js/cbp-ds-bundle.js' // name of compiled js build file
+    path: path.resolve(__dirname, '../ds-ux-guidelines/dist'), // directory location for output files
+    filename: 'js/cbp-ds-bundle.js' // name of compiled js build file 
   },
-  // watch: true, // (watches changes in any of the resolved files)
+
   module: {
     rules: [
       {
       test: /\.(sa|sc|c)ss$/, // test covers both sass and css file types
       use: [ 
-          MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader, 
           {
             loader: 'css-loader', // interprets @import and url() like import/require() and will resolve them
           },
@@ -51,11 +54,6 @@ module.exports = {
             options: {
               sourceMap: true,
               implementation: require('sass'), // Prefer `dart-sass`
-              sassOptions: {
-                // fiber: false,
-                // using fiber support by default, allowing jumps between
-                // call stacks from a single thread if needed.
-              },
             }
           },
         ]
@@ -79,54 +77,57 @@ module.exports = {
     new webpack.ProgressPlugin(), // show build progress details
     new CleanWebpackPlugin(),  // keeps track of all files in dist/
 
+    new MiniCssExtractPlugin({ // extracts CSS into separate files per JS file that contains CSS
+      // also generates a min.css.map file
+      // note: this is not creating the unminified .css file, another tool does that for now
+      ignoreOrder: false, // enable to remove warnings about any possible conflict
+      //CSS in separate location for distribution
+      filename: "css/cbp-ds.min.css",
+    }),
+
     // an HTML template is required to serve the bundle
     new HtmlWebpackPlugin({
-      entry: '../ds-ux-guidelines/index.html',
       hash: false, // set to 'true' if needing unique ID's at end of file name
       title: 'Style Guide - CBP Style Guide',
-      template: '../ds-ux-guidelines/index.html',
-      inject: true,
+      template: '../ds-ux-guidelines/index.html', // using default template engine
+      filename: 'index.html',  // sets up new file
+      inject: true, // default setting to place bundle.js file at bottom
+      showErrors: true,
       minify: {
           removeComments: false,
           collapseWhitespace: false
       }
   }),
-    new MiniCssExtractPlugin({ // extracts CSS into separate files per JS file that contains CSS
-      // also generates a min.css.map file
-      // note: this is not creating the unminified .css file, another tool does that for now
-      ignoreOrder: false, // enable to remove warnings about any possible conflict
-      filename: 'css/cbp-ds.min.css'
-    }),
+  new HtmlWebpackPlugin({
+    template: '../ds-ux-guidelines/foundation/icons.html', // telling where to get the code to build the file from
+    hash: false, // set to 'true' if needing unique ID's at end of file name
+    filename: 'foundation/icons.html',
+    inject: true, 
+    showErrors: true,
+    minify: {
+        removeComments: false,
+        collapseWhitespace: false
+    }
+}),
 
-    // new OptimizeCssAssetsPlugin({ // optimizes/minifies all CSS assets
-    //   assetNameRegExp: /\.css$/g,
-    //   cssProcessor: require('cssnano'),
-    //   cssProcessorPluginOptions: {
-    //     preset: ['default', { 
-          
-    //       discardComments: { 
-    //       removeAll: true },
+// new CopyPlugin([
+//   { from: 'css/cbp-ds.min.css', to: '../.././ds-css/dist/css/' },
+// ]),
 
-    //       cssDeclarationSorter: {
-    //         order: 'alphabetically' // formats properties in alphabetical order
-    //       },
-    //       normalizeWhitespace: true
+// new CopyPlugin([
+//   { from: 'css/cbp-ds.min.css', to: '.././ds-css/dist/css/' },
+// ]),
 
-    //     }],
-    //   },
-    //   canPrint: true
-    // }),
+new HtmlWebpackTagsPlugin({ tags: ['../css/styles.css', '../js/scripts.js'], append: true }), 
 
-    new StylelintPlugin({
+new StylelintPlugin({
 
   "options": {
     config: '.stylelintrc',
     files: '**/*.s?(a|c)ss',
-    // files: './src/styles/custom/**/*.scss',
-    // files: ['./src/styles/custom/**/*.scss', '../ds-ux-guidelines/sass/styles.scss'],
     formatter: 'string',
     cache: true, // only changed files get linted
-    cacheLocation: './tests/',
+    // cacheLocation: './tests/',
     reportInvalidScopeDisables: true,
     // ignorePath: './.stylelintignore',
     syntax: 'scss',
@@ -136,5 +137,6 @@ module.exports = {
   }
 
   })
+
 ]
 }
