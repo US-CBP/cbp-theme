712db73cb8597005a952cde024dbc0cfbcb4b570
Ray Robertson <ray760@yahoo.com>
2019-11-15 13:54:44 -0500

    Added button to hide main header and expand sub header in responsive view.

--- ds-ux-guidelines/js/scripts.js
+++ ds-ux-guidelines/js/scripts.js
@@ -2,8 +2,37 @@
  const navBar = document.querySelector('.cbp-side-nav');
  const mainGrid = document.querySelector('.cbp-ds-grid');
 
+ /* App mainmenu open/close START */
+const mainHeader = document.querySelector('.cbp-header__main'); 
+const hideBtn = document.querySelector('.hide-btn');
+
+hideBtn.addEventListener('click', (e) => {
+
+  if(mainHeader.classList.contains('cbp-header__main--hidden')) {
+    mainHeader.classList.remove('cbp-header__main--hidden');
+  } else {
+    mainHeader.classList.add('cbp-header__main--hidden');
+  }
+
+});
+
+ /* App mainmenu open/close END */
+
+ /* App submenu open/close START */
+ const subHeader = document.querySelector('.cbp-header__sub');
+ const subBtn = document.querySelector('.sub-menu-btn');
+
+ subBtn.addEventListener('click', (e) => {
+  if(subHeader.classList.contains('cbp-header__sub--open')) {
+    subHeader.classList.remove('cbp-header__sub--open');
+  } else {
+    subHeader.classList.add('cbp-header__sub--open');
+  }
+ });
+ /* App submenu open/close END */
+
  /* Hamburger menu open/close code START */
- 
+ if (hamburger) {
  hamburger.addEventListener( 'click', function(e) {
    
   if(navBar.classList.contains('cbp-side-nav--hide')) {
@@ -15,5 +44,5 @@
   }
 
  });
-
+}
  /* Hamburger menu open/close code START */
