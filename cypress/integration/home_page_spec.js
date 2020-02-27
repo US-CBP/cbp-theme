e349f68d8bc90315e40db789912bf98cea254ef9
Ray Robertson <ray760@yahoo.com>
2020-02-27 13:46:09 -0500

    Pr branch (#323)
    * Updated style guide readme
    
    * 1. Updated cbp-theme package.json file's tests:e2e, local:run, website:prepare, and website:build scripts
    2. Updated cypress.json file's baseUrl
    3. Updated cypress/intergration home_page_spec.js to reflect index page changes
    
    * Removed part of pa11y report script.
    
    * package-lock.json file updated package versions on npm i
    
    * Updated folder names.
    1. Changed folder name ds-ux-guidlines-gatsby to ds-ux-guidelines.
    2. Changed folder name ds-ux-guidlines to ds-ux-guidelines-v1.
    
    * 1. Added pa11y package to build.
    2. Added and updated .pa11yci file.
    3. Updated .gitignore to ignore pa11y folder.
    
    * Added Husky package and pre-commit hook to package.json file.
    
    * 1. Installed stylelint packages.
    2. Added .stylelintrc, and pa11y-ci-README.md files.
    3. Updated package.json file with pa11y scripts.
    
    * 1. Updated scripts in package.json file for stylelint
    2. Updated .gitignore with lint folder.
    
    * 1. Updated scripts in package.json file for stylelint
    2. Updated .gitignore with lint folder.
    
    * Removed end to endtesting script in travis.yml file.
    
    * Edited travis.yml to remove space.
    
    * Updated website:prepare script with correct path.
    
    * Updated package.json name, descriptiion, and deleted private.

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,6 +3,6 @@ describe('CBP Design System Home Page', function () {
   it('successfully loads', function () {
     cy.visit('/')
 
-    cy.get('h3').should('contain', 'CBP Style Guide Library')
+    cy.get('h1').should('contain', 'Home Page')
   })
 })
