fab1570ce958f37663497b0a1fdd1041ef1fe0b5
Minjung Bin <minjungbin@gmail.com>
2020-03-30 16:34:58 -0400

    initial tabs commit

--- ds-ux-guidelines/src/ds-components/content-header/contentHeader.js
+++ ds-ux-guidelines/src/ds-components/content-header/contentHeader.js
@@ -1,42 +1,39 @@
 /* import { Link } from "gatsby" */
 import PropTypes from "prop-types"
 import React from "react"
+import { Tab, Tabs, TabList } from "react-tabs"
+// import "react-tabs/style/react-tabs.scss"
 
-
-//So far there is only contentType = component or 'none' (has no tabs). 
-//Need to ditch the if/else statement if we implement additional types
+// import menuTab from "../ds-tabs/tabs"
 
 const ContentHeader = ({ title, contentType }) => {
-  if(contentType === "component"){
+  if (contentType === "component") {
     return (
       <div className="cbp-content__header">
         <h1 className="cbp-masthead-1 heading-pad-temp">{title}</h1>
-        <div className="tabs is-boxed">
-          <ul>
-            <li>
-              <a href="#noID">
-                <i className="fas fa-code fa-xs">&nbsp;</i>
+        <Tabs className="tabs is-boxed" style={{ margin: "0 20px" }}>
+          <TabList>
+            <Tab selectedClassName="is-active">
+              <a href="#Code" className="tab--text">
                 Code
               </a>
-            </li>
-            <li>
-              <a href="#noID">
-                <i className="fas fa-paint-brush fa-xs">&nbsp;</i>
+            </Tab>
+            <Tab selectedClassName="is-active">
+              <a className="tab--text" href="#Design Guidelines">
                 Design Guidelines
               </a>
-            </li>
-          </ul>
-        </div>
+            </Tab>
+          </TabList>
+        </Tabs>
+        {/* <menuTab tabContent={["Code", "Design Guidelines"]} /> */}
       </div>
-    );
-
+    )
   } else {
-    //Omit the Code/Design tabs. 
     return (
       <div className="cbp-content__header">
         <h1 className="cbp-masthead-1 heading-pad-temp">{title}</h1>
       </div>
-    );
+    )
   }
 }
 
