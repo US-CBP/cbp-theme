a79c9490044c45cb44394948095f5c3c282526eb
Travis CI User <travis@example.org>
2020-04-01 18:37:38 +0000

    rebuild pages at 7a91d11

---
+++ design-system/component---src-pages-index-js-d863a477bf9dc11975d6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("Dt0B"),r=a("zJqo"),m=a("3NRZ");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(m.a,{title:"CBP Design System | Home"}),l.a.createElement(r.a,{title:"Home Page"}),l.a.createElement("article",null,l.a.createElement("h1",{className:"cbp-masthead-1"},"CBP ",l.a.createElement("b",null,"Design System")," Library")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d863a477bf9dc11975d6.js.map
