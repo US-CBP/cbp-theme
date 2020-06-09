f6e294e253c8b006e1a8d68389bde734489c58d5
Travis CI User <travis@example.org>
2020-06-09 22:35:02 +0000

    rebuild pages at bfecdb6

--- design-system/component---src-pages-404-js-0f33c7fb517c15ca5f71.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Dt0B"),o=n("3NRZ");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-0f33c7fb517c15ca5f71.js.map
