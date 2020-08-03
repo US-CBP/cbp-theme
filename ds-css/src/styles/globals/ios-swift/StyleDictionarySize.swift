08cfa7b3fccd425fe8b0f586908dcc2360d505f1
Minjung Bin <minjungbin@gmail.com>
2020-08-03 16:50:49 -0400

    added design tokens with color draft in ds-css

---
+++ ds-css/src/styles/globals/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,21 @@
+
+//
+// StyleDictionarySize.swift
+//
+// Do not edit directly
+// Generated on Mon, 03 Aug 2020 20:48:28 GMT
+//
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let fontBody = CGFloat(224.00)
+    public static let fontHeading1 = CGFloat(448.00)
+    public static let fontHeading2 = CGFloat(400.00)
+    public static let fontHeading3 = CGFloat(352.00)
+    public static let fontHeading4 = CGFloat(320.00)
+    public static let fontHeading5 = CGFloat(320.00)
+    public static let fontHeading6 = CGFloat(256.00)
+    public static let fontMasthead1 = CGFloat(576.00)
+    public static let fontMasthead2 = CGFloat(512.00)
+}
---
+++ ds-css/src/styles/globals/ios/fontSizes.m
@@ -0,0 +1,21 @@
+
+//
+// fontSizes.m
+//
+// Do not edit directly
+// Generated on Mon, 03 Aug 2020 20:48:28 GMT
+//
+
+#import "FontSize.h"
+
+
+
+float const SizeFontBody = 224.00f;
+float const SizeFontHeading6 = 256.00f;
+float const SizeFontHeading5 = 320.00f;
+float const SizeFontHeading4 = 320.00f;
+float const SizeFontHeading3 = 352.00f;
+float const SizeFontHeading2 = 400.00f;
+float const SizeFontHeading1 = 448.00f;
+float const SizeFontMasthead2 = 512.00f;
+float const SizeFontMasthead1 = 576.00f;
