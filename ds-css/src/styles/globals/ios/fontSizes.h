e224add902c0662a835afba20ce7a0bffd4a9f57
Minjung <minjungbin@gmail.com>
2020-08-25 14:04:34 -0400

    Ds css refactor (#377)
    * sass folder restructuring initial commit
    
    * added design tokens with color draft in ds-css
    
    * added current variables
    
    * initial commit- changed file structure
    
    * add css variable output in config.json
    
    * merged in design tokens
    
    * refactored @import rule to @use/@forward in ds-css, cleaned up file structure, and added design tokens (currently not used as color variables are still WIP)

---
+++ ds-css/src/styles/globals/ios/fontSizes.h
@@ -0,0 +1,20 @@
+
+// fontSizes.h
+//
+// Do not edit directly
+// Generated on Mon, 03 Aug 2020 20:48:28 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontBody;
+extern float const SizeFontHeading6;
+extern float const SizeFontHeading5;
+extern float const SizeFontHeading4;
+extern float const SizeFontHeading3;
+extern float const SizeFontHeading2;
+extern float const SizeFontHeading1;
+extern float const SizeFontMasthead2;
+extern float const SizeFontMasthead1;
