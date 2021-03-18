33330cd9959c52340cfd77eb243ece4604084283
Ray Robertson <ray760@yahoo.com>
2021-03-18 12:41:41 -0400

    Reworked sizing and html structure of filter draw. Completed functionality for sliding in and out.

--- ds-css-v2/demo-components/slide-out-draw/index.js
+++ ds-css-v2/demo-components/slide-out-draw/index.js
@@ -1,8 +1,17 @@
 const accordion = document.querySelectorAll('.cbp-filter-cmpt button');
-const activeBtn = document.querySelector('.cbp-filter-cmpt .active-parent')
+const activeBtn = document.querySelector('.cbp-filter-cmpt .active-parent');
+
+const CBPSlider = document.querySelector('.cbp-slide-draw');
+const CBPSliderCloseBtn = document.querySelector('.cbp-slide-draw__close-btn');
 
 accordion.forEach(item => {
   item.addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('active-parent');
   });
 });
+
+const toggleSlider = () => {
+  CBPSlider.classList.toggle('cbp-slide-draw--closed');
+}
+
+CBPSliderCloseBtn.addEventListener('click', () => { toggleSlider() });
