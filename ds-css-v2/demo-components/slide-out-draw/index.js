222fa5c81d179b1237b6b4907f692cc2a4a9d200
Ray Robertson <ray760@yahoo.com>
2021-03-19 14:58:35 -0400

    Card archetype filter (#396)
    * 2.3.7
    
    * Universal Header component initial setup.
    
    * Intial slide out draw setup.
    
    * Completed intial card filter archetype development.
    
    * Reworked sizing and html structure of filter draw. Completed functionality for sliding in and out.
    
    * Added slider draw functionality to demo component slider.
    
    * Updated filter draw with lightbox background, and left blue border
    
    * Updated js to fix active button issue
    
    * Updated active button color for Safari.

---
+++ ds-css-v2/demo-components/slide-out-draw/index.js
@@ -0,0 +1,22 @@
+const accordion = document.querySelectorAll('.cbp-filter-cmpt .cbp-acc__btn');
+const activeBtn = document.querySelector('.cbp-filter-cmpt .active-parent');
+
+const CBPSlider = document.querySelector('.cbp-slide-draw');
+const CBPSliderCloseBtn = document.querySelector('.cbp-slide-draw__close-btn');
+
+const openFilter = document.querySelector('.filter-open');
+const lightBox = document.querySelector('.light-box');
+
+accordion.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('active-parent');
+  });
+});
+
+const toggleSlider = () => {
+  CBPSlider.classList.toggle('cbp-slide-draw--closed');
+  lightBox.classList.toggle('light-box--hidden');
+}
+
+CBPSliderCloseBtn.addEventListener('click', () => { toggleSlider() });
+openFilter.addEventListener('click', () => { toggleSlider() });
