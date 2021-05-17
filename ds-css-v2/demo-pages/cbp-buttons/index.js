1c70b243e7f0bf8c6e1eb19836c437ba2ac83010
Ray Robertson <ray760@yahoo.com>
2021-05-17 12:49:21 -0400

    Added mobile version of Profile archetpye and initial cbp button recommendations.

---
+++ ds-css-v2/demo-pages/cbp-buttons/index.js
@@ -0,0 +1,49 @@
+const disableBtns = document.querySelectorAll('.disable-btn');
+const justTextBtns = document.querySelectorAll('.just-text-btn');
+const justIconBtns = document.querySelectorAll('.just-icon-btn');
+
+const returnDisplay = (el) => {
+  const currentState = window.getComputedStyle(el, null).getPropertyValue("display")
+
+  if(currentState !== 'none') {
+    return 'none';
+  } else {
+    return 'inline';
+  }
+};
+
+disableBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const parent = e.target.parentElement;
+    const targetBtn = parent.firstElementChild;
+    targetBtn.disabled = !targetBtn.disabled;
+  })
+})
+
+justTextBtns.forEach( btn => {
+  btn.addEventListener('click', e => {
+    const parent = e.target.parentElement;
+    const btnTxt = parent.querySelector('.cbp-text-button');
+    
+    btnTxt.style.display = returnDisplay(btnTxt);
+
+  })
+})
+
+justIconBtns.forEach( btn => {
+  btn.addEventListener('click', e => {
+    const parent = e.target.parentElement;
+    const btnIcon = parent.querySelector('.cbp-btn > .fas');
+    const btnTxt = parent.querySelector('.cbp-text-button');
+
+
+    btnIcon.style.display = returnDisplay(btnIcon);
+    
+    if(returnDisplay(btnIcon) === 'none') {
+      btnTxt.style.marginLeft = '8px';
+    } else {
+      btnTxt.style.marginLeft = 0;
+    }
+
+  })
+})
