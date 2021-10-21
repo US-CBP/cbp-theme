fc7f7c5ac911a6bdacd076fd1061a5646501c204
Danny Kim <dannyk3941@gmail.com>
2021-10-21 11:21:17 -0400

    chore: alignment of button when text is long

--- ds-css-v2/js/cbp-components-js/cbpExpand.js
+++ ds-css-v2/js/cbp-components-js/cbpExpand.js
@@ -1,8 +1,8 @@
-const expandComponent = document.querySelectorAll('.cbp-expand__title')
+const expandTitles = document.querySelectorAll(".cbp-expand__title");
 
-expandComponent.forEach( component => {
-  component.addEventListener('click', (item) => {
-    const expandParent = item.target.closest('.cbp-expand');
-    expandParent.classList.toggle('active')
-  })
-})
+expandTitles.forEach((title) => {
+  title.addEventListener("click", (item) => {
+    const expandParent = item.target.closest(".cbp-expand");
+    expandParent.classList.toggle("active");
+  });
+});
