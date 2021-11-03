7e7beb7e81e593eb46e08ffb4fb7c12710b95383
Danny Kim <dannyk3941@gmail.com>
2021-11-03 15:23:16 -0400

    chore: add buttons to toggle danger and simple modal

--- ds-css-v2/js/cbp-components-js/cbpModal.js
+++ ds-css-v2/js/cbp-components-js/cbpModal.js
@@ -13,3 +13,26 @@ window.onclick = (e) => {
     closeModal()
   }
 }
+
+// For demo purposes only
+
+const dangerBtn = document.querySelector(".header-danger");
+const simpleBtn = document.querySelector(".simple-modal")
+const modalHeader = document.querySelector(".cbp-modal__header");
+const btns = document.querySelectorAll(".cbp-btn");
+
+dangerBtn.addEventListener('click', (e) => {
+  modalHeader.classList.toggle('cbp-modal__header--danger');
+  btns[btns.length - 1].classList.toggle('cbp-btn--danger')
+})
+
+simpleBtn.addEventListener('click', (e) => {
+  const hr = document.querySelector(".cbp-modal__content hr");
+  if (modalHeader.style.display === "none") {
+    modalHeader.style.display = "flex"
+    hr.style.display = "block"
+  } else {
+    modalHeader.style.display = "none"
+    hr.style.display = "none"
+  }
+})
