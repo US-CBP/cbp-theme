ab7b91ec5611153e99095243f78f2821baca7c8d
Danny Kim <dannyk3941@gmail.com>
2021-11-04 16:03:01 -0400

    fix: use data attributes for modals instead

--- ds-css-v2/js/cbp-components-js/cbpModal.js
+++ ds-css-v2/js/cbp-components-js/cbpModal.js
@@ -1,38 +1,52 @@
-const modal = document.querySelector(".cbp-modal");
+const modalTarget = document.querySelectorAll('[data-modal-target]');
+const modalClose = document.querySelectorAll('[data-modal-close]');
 
-const openModal = () => {
-  modal.style.display = "block";
-}
+// Open Modal
+modalTarget.forEach(modal => {
+  modal.addEventListener('click', (e) => {
+    const modal = document.querySelector(`#${e.target.dataset.modalTarget}`);
+    modal.style.display = "block";
+  })
+})
 
-const closeModal = () => {
-  modal.style.display = "none";
-}
+// Close Modal
+modalClose.forEach(close => {
+  close.addEventListener('click', (e) => {
+    const closestModal = e.target.closest(".cbp-modal");
+    closestModal.style.display = "none";
+  })
+})
 
+// Close Modal from Overlay
 window.onclick = (e) => {
-  if (e.target == modal) {
-    closeModal()
+  if (e.target.classList.contains('cbp-modal')) {
+    e.target.style.display = "none";
   }
 }
 
 // For demo purposes only
 
-const dangerBtn = document.querySelector(".header-danger");
-const simpleBtn = document.querySelector(".simple-modal")
+const dangerBtn = document.querySelectorAll(".header-danger");
+const simpleBtn = document.querySelectorAll(".simple-modal");
 const modalHeader = document.querySelector(".cbp-modal__header");
-const btns = document.querySelectorAll(".cbp-btn");
+const modalFooter = document.querySelector(".cbp-modal__footer");
 
-dangerBtn.addEventListener('click', (e) => {
-  modalHeader.classList.toggle('cbp-modal__header--danger');
-  btns[btns.length - 1].classList.toggle('cbp-btn--danger')
+dangerBtn.forEach(item => {
+  item.addEventListener('click', (e) => {
+    modalHeader.classList.toggle('cbp-modal__header--danger');
+    modalFooter.children[modalFooter.children.length - 1].classList.toggle('cbp-btn--danger')
+  })
 })
 
-simpleBtn.addEventListener('click', (e) => {
-  const hr = document.querySelector(".cbp-modal__content hr");
-  if (modalHeader.style.display === "none") {
-    modalHeader.style.display = "flex"
-    hr.style.display = "block"
-  } else {
-    modalHeader.style.display = "none"
-    hr.style.display = "none"
-  }
+simpleBtn.forEach(item => {
+  item.addEventListener('click', (e) => {
+    const hr = document.querySelector(".cbp-modal__content hr");
+    if (modalHeader.style.display === "none") {
+      modalHeader.style.display = "flex"
+      hr.style.display = "block"
+    } else {
+      modalHeader.style.display = "none"
+      hr.style.display = "none"
+    }
+  })
 })
