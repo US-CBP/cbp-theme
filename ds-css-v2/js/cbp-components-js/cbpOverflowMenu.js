a8576953997d00c9a439dbffe51dc2a5d77a2440
Danny Kim <dannyk3941@gmail.com>
2021-12-02 12:26:12 -0500

    chore: move things around and had visual box around component

--- ds-css-v2/js/cbp-components-js/cbpOverflowMenu.js
+++ ds-css-v2/js/cbp-components-js/cbpOverflowMenu.js
@@ -1,7 +1,56 @@
-function OverflowMenu() {
-  console.log(this);
+const openMenus = []
+
+const utilMenu = {
+  // Returns the current menu that is open
+  getCurrentMenu: function() {
+    return openMenus[openMenus.length - 1];
+  }
 }
 
+function OverflowMenu(menuId) {
+  this.menuNode = document.getElementById(menuId);
+
+  openMenus.push(this);
+
+  this.menuNode.classList.toggle('active')
+}
+
+OverflowMenu.prototype.close = function () {
+  this.menuNode.classList.toggle('active');
+}
+
+const radioBtns = document.querySelectorAll('input[name="position"]')
+
+radioBtns.forEach(btn => {
+  btn.onclick = function() {
+    const menu = document.getElementById('menu1');
+    
+    menu.classList.remove(menu.className.split(' ')[1])
+
+    switch (btn.value) {
+      case 'bl':
+        menu.classList.add('cbp-overflow-menu--bottom-left');
+        break;
+      case 'tr':
+        menu.classList.add('cbp-overflow-menu--top-right');
+        break;
+      case 'br':
+        menu.classList.add('cbp-overflow-menu--bottom-right');
+        break;
+      case 'tl':
+        menu.classList.add('cbp-overflow-menu--top-left');
+        break;
+      default:
+        break;
+    }
+  }
+})
+
 window.openOverflowMenu = function (menuId) {
-  const overflowMenu = new OverflowMenu();
+  const overflowMenu = new OverflowMenu(menuId);
+};
+
+window.closeOverflowMenu = function () {
+  const currentMenu = utilMenu.getCurrentMenu();
+  currentMenu.close();
 }
