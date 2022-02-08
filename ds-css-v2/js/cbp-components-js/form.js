94ae72b8427ab5128f599485ebf7772349c9c74e
Danny Kim <dannyk3941@gmail.com>
2022-02-08 16:18:10 -0500

    feat: hash field look & feel and a  tiny bit of js

---
+++ ds-css-v2/js/cbp-components-js/form.js
@@ -0,0 +1,15 @@
+const { children } = document.querySelector(".cbp-form__password");
+
+const [input, btn] = children;
+
+console.log(btn.firstElementChild.className);
+
+btn.addEventListener("click", (e) => {
+  if (input.type === "password") {
+    input.type = "text";
+    btn.firstElementChild.className = "fas fa-eye";
+  } else {
+    input.type = "password";
+    btn.firstElementChild.className = "fas fa-eye-slash";
+  }
+})
