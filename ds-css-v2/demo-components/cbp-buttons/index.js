7bce5e34f0f04603ad7dc20fea8c2de0adba677a
dannyk3941 <52807859+dannyk3941@users.noreply.github.com>
2022-04-19 15:34:10 -0400

    Update button component (#420)
    * fix: initial update to button
    
    * chore: move classes for each type of button
    
    * chore: floating action buttons
    
    * feat: ghost buttons
    
    * feat: solid square buttons
    
    * feat: added text segmented and cta button
    
    * chore: remove unneeded hover mixin
    
    * fix: pseudo classes for outline buttons and segmented buttons
    
    * chore: add active states for obuttons
    
    * chore: remove icon check box for square buttons
    
    * feat: multi-selection for segment buttons
    
    * fix: single select for segment buttons and partial demo archetypes updates
    
    * chore: styling on buttons in demo pages
    
    * chore: this commit is for demo purposes only, please delete this code before merge
    
    * chore: removed demo for screenshot
    
    * chore: ripple animation on all buttons
    
    * chore: update active classes for buttons
    
    * chore: add active states to solid buttons
    
    * chore: outline active css
    
    * chore: add ghost active state css
    
    * chore: separate mixins to its own folder and update classes for square buttons
    
    * chore: styling for links, change class name and add js for disabling
    
    * chore: rearrange mixins and comment active button states
    
    * chore: re-arrange and dry up square button classes
    
    * chore: fixed disable and removing icon checkbox
    
    * fix: color of danger ghost hover background
    
    * fix: disabled ghost button styling
    
    * fix: focus styling for deselect of multi-segment btn
    
    * chore: switch hex values to tokens
    
    * fix: remove active hover states
    
    * fix: remove unneeded classes in html and fix disabled hover styling
    
    * chore: change name to partial for button and link

--- ds-css-v2/demo-components/cbp-buttons/index.js
+++ ds-css-v2/demo-components/cbp-buttons/index.js
@@ -1,6 +1,11 @@
 const disableBtns = document.querySelectorAll('.disable-btn');
 const justTextBtns = document.querySelectorAll('.just-text-btn');
 const justIconBtns = document.querySelectorAll('.just-icon-btn');
+const activeBtns = document.querySelectorAll('.active-btn');
+const outlineActiveBtns = document.querySelectorAll('.outline-active-btn');
+const ghostActiveBtns = document.querySelectorAll('.ghost-active-btn');
+const disabledLinks = document.querySelectorAll('[aria-disabled="true"]');
+const linkDisableBtn = document.querySelector('#link-disable');
 
 const returnDisplay = (el) => {
   const currentState = window.getComputedStyle(el, null).getPropertyValue("display")
@@ -20,38 +25,118 @@ disableBtns.forEach((btn) => {
   })
 })
 
-justTextBtns.forEach( btn => {
-  btn.addEventListener('click', e => {
-    const parent = e.target.parentElement;
-    const btnIcon = parent.querySelector('.cbp-btn > .fas');
-    const btnTxt = parent.querySelector('.cbp-text-button');
-    
-    btnTxt.style.display = returnDisplay(btnTxt);
-
-    if(returnDisplay(btnTxt) === 'none') {
-      console.log(returnDisplay(btnTxt))
-      btnIcon.style.marginLeft = '20px';
-    } else {
-      btnIcon.style.marginLeft = 0;
-    }
-
-  })
-})
-
 justIconBtns.forEach( btn => {
   btn.addEventListener('click', e => {
     const parent = e.target.parentElement;
     const btnIcon = parent.querySelector('.cbp-btn > .fas');
-    const btnTxt = parent.querySelector('.cbp-text-button');
 
-
-    btnIcon.style.display = returnDisplay(btnIcon);
-    
-    if(returnDisplay(btnIcon) === 'none') {
-      btnTxt.style.marginLeft = '8px';
+    if (btn.checked) {
+      btnIcon.style.display = "none";
     } else {
-      btnTxt.style.marginLeft = '20px';
+      btnIcon.style.display = "inline";
     }
-
   })
 })
+
+activeBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const parent = e.target.parentElement;
+    const targetBtn = parent.firstElementChild;
+    targetBtn.classList.toggle("cbp-btn--active");
+  })
+})
+
+outlineActiveBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const parent = e.target.parentElement;
+    const targetBtn = parent.firstElementChild;
+    targetBtn.classList.toggle("cbp-btn--outline-active");
+  })
+})
+
+ghostActiveBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const parent = e.target.parentElement;
+    const targetBtn = parent.firstElementChild;
+    targetBtn.classList.toggle("cbp-btn--ghost-active");
+  })
+})
+
+disabledLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    if (e.target.nodeName === 'A' && e.target.getAttribute('aria-disabled') == 'true') {
+      e.preventDefault();
+    }
+  })
+})
+
+linkDisableBtn.addEventListener("click", e => {
+  const parent = e.target.parentElement;
+  const targetBtn = parent.firstElementChild;
+  
+  targetBtn.ariaDisabled = linkDisableBtn.checked
+  targetBtn.classList.toggle("cbp-link__inline--disabled");
+})
+
+const singleSegment = document.querySelectorAll('[data-segment-button="single"]');
+const multiSegment = document.querySelectorAll('[data-segment-button="multi"]');
+
+multiSegment.forEach(segment => {
+  let selected = [];
+
+  const { children } = segment;
+
+  for (const child of children) {
+    child.addEventListener('click', e => {
+      selected.push(e.target);
+      e.target.classList.toggle("active");
+    })
+  }
+})
+
+singleSegment.forEach(segment => {
+  let selected;
+  
+  const { children } = segment;
+
+  for (const child of children) {
+    child.addEventListener('click', e => {
+      if (typeof selected === "undefined") {
+        selected = e.currentTarget;
+        selected.classList.add("active");
+      } else if (e.currentTarget === selected) {
+        selected.classList.remove("active");
+      }
+
+      if (e.currentTarget != selected) {
+        selected.classList.remove("active");
+        selected = e.currentTarget;
+        selected.classList.add("active");
+      }
+    })
+  }
+})
+
+/* Ripple animation effect on all buttons */
+const createRipple = (e) => {
+  const button = e.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${e.offsetX - radius}px`;
+  circle.style.top = `${e.offsetY - radius}px`;
+  circle.classList.add("ripple"); 
+
+  button.appendChild(circle);
+
+  circle.addEventListener('animationend', () => button.removeChild(circle));
+}
+
+const buttons = document.getElementsByTagName("button");
+
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
