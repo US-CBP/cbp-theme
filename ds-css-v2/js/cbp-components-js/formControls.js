fdef93a2aac5d4811b0c765997af8a5b5917e55a
Danny Kim <dannyk3941@gmail.com>
2022-04-22 16:55:40 -0400

    chore: disable and inline options for form-controls demo

--- ds-css-v2/js/cbp-components-js/formControls.js
+++ ds-css-v2/js/cbp-components-js/formControls.js
@@ -1,4 +1,31 @@
-
 const indCheck = document.getElementById('indy');
 
 indCheck.indeterminate = true;
+
+const setInline = (e, wrapperId) => {
+  const { target: { checked } } = e;
+  const wrapper = document.getElementById(wrapperId);
+
+  if (checked) {
+    wrapper.classList.toggle('cbp-form__control--inline');
+  } else {
+    wrapper.classList.toggle('cbp-form__control--inline');
+  }
+}
+
+const setControlDisabled = (e, wrapperId) => {
+  const { target: { checked } } = e;
+  const wrapper = document.getElementById(wrapperId);
+  const allInputs = wrapper.querySelectorAll('.cbp-form__checkbox,.cbp-form__radio');
+  const { children } = wrapper;
+
+  if (checked) {
+    allInputs.forEach(input => {
+      input.disabled = true;
+    })
+  } else {
+    allInputs.forEach(input => {
+      input.disabled = false;
+    })
+  }
+}
