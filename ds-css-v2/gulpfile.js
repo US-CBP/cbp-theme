aaf91705673d38de1a33b94000d3e1a665313dcc
Danny Kim <dannyk3941@gmail.com>
2022-05-11 18:22:21 -0400

    fix: gulpfile refactor and add gulp-babel

--- ds-css-v2/gulpfile.js
+++ ds-css-v2/gulpfile.js
@@ -1,59 +1,15 @@
-/*
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-========================================
-========================================
-========================================
-----------------------------------------
-USWDS SASS GULPFILE
-----------------------------------------
-*/
+const { series, src, watch, dest } = require("gulp");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const pkg = require("./node_modules/uswds/package.json");
+const postcss = require("gulp-postcss");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass")(require("sass"));
+const babel = require("gulp-babel");
+const uswds = require("./node_modules/uswds-gulp/config/uswds");
 
-const autoprefixer = require('autoprefixer');
-const csso = require('postcss-csso');
-const gulp = require('gulp');
-const pkg = require('./node_modules/uswds/package.json');
-const postcss = require('gulp-postcss');
-const replace = require('gulp-replace');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const uswds = require('./node_modules/uswds-gulp/config/uswds');
-
-sass.compiler = require('sass');
-
-/*
-----------------------------------------
-PATHS
-----------------------------------------
-- All paths are relative to the
-  project root
-- Don't use a trailing `/` for path
-  names
-----------------------------------------
-*/
-
-// Project Sass source directory
-const PROJECT_SASS_SRC = 'styles/sass';
-
-// Images destination
-const IMG_DEST = 'assets/images';
-
-// Fonts destination
-const FONTS_DEST = 'assets/fonts';
-
-// Javascript destination
-const JS_DEST = 'js';
-
-// Compiled CSS destination
-const CSS_DEST = 'dist/';
-// const CSS_DEST = 'styles/css';
-
-// Site CSS destination
-// Like the _site/assets/css directory in Jekyll, if necessary.
-// If using, uncomment line 106
-const SITE_CSS_DEST = './path/to/site/css/destination';
+const PROJECT_SASS_SRC = "styles/sass";
+const CSS_DEST = "dist/";
 
 /*
 ----------------------------------------
@@ -61,71 +17,45 @@ TASKS
 ----------------------------------------
 */
 
-gulp.task('copy-uswds-setup', () => {
-  return gulp
-    .src(`${uswds}/scss/theme/**/**`)
-    .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
-});
+function buildJs() {
+  return src("js/main.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(dest("dist"));
+}
 
-gulp.task('copy-uswds-fonts', () => {
-  return gulp.src(`${uswds}/fonts/**/**`).pipe(gulp.dest(`${FONTS_DEST}`));
-});
-
-gulp.task('copy-uswds-images', () => {
-  return gulp.src(`${uswds}/img/**/**`).pipe(gulp.dest(`${IMG_DEST}`));
-});
-
-gulp.task('copy-uswds-js', () => {
-  return gulp.src(`${uswds}/js/**/**`).pipe(gulp.dest(`${JS_DEST}`));
-});
-
-gulp.task('build-sass', function (done) {
-  var plugins = [
-    // Autoprefix
-    autoprefixer({
-      cascade: false,
-      grid: true,
-    }),
-    // Minify
-    csso({ forceMediaMerge: false }),
+function buildSass() {
+  const plugins = [
+    autoprefixer({ cascade: false, grid: true }), // Autoprefix
+    csso({ forceMediaMerge: false }) // Minify
   ];
-  return (
-    gulp
-      .src([`${PROJECT_SASS_SRC}/*.scss`])
-      .pipe(sourcemaps.init({ largeFile: true }))
-      .pipe(
-        sass.sync({
-          includePaths: [
-            `${PROJECT_SASS_SRC}`,
-            `${uswds}/scss`,
-            `${uswds}/scss/packages`,
-          ],
-        })
-      )
-      .pipe(replace(/\buswds @version\b/g, 'based on uswds v' + pkg.version))
-      .pipe(postcss(plugins))
-      .pipe(sourcemaps.write('.'))
-      // uncomment the next line if necessary for Jekyll to build properly
-      //.pipe(gulp.dest(`${SITE_CSS_DEST}`))
-      .pipe(gulp.dest(`${CSS_DEST}`))
-  );
-});
 
-gulp.task(
-  'init',
-  gulp.series(
-    'copy-uswds-setup',
-    'copy-uswds-fonts',
-    'copy-uswds-images',
-    'copy-uswds-js',
-    'build-sass'
-  )
-);
+  return src([`${PROJECT_SASS_SRC}/*.scss`], { sourcemaps: true, largeFile: true })
+    .pipe(
+      sass.sync({
+        includePaths: [
+          `${PROJECT_SASS_SRC}`,
+          `${uswds}/scss`,
+          `${uswds}/scss/packages`,
+        ],
+      })
+    )
+    .pipe(replace(/\buswds @version\b/g, "based on uswds v" + pkg.version))
+    .pipe(postcss(plugins))
+    .pipe(dest(`${CSS_DEST}`, { sourcemaps: '.', largeFile: true }));
+}
 
-gulp.task('watch-sass', function () {
-  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series('build-sass'));
-});
+function defaultTasks() {
+  watch("js/main.js", buildJs);
+  watch(`${PROJECT_SASS_SRC}/**/*.scss`, buildSass);
+}
 
-gulp.task('watch', gulp.series('build-sass', 'watch-sass'));
-
-gulp.task('default', gulp.series('watch'));
+module.exports = {
+  build: series(buildJs, buildSass),
+  buildJs,
+  buildSass,
+  default: defaultTasks,
+}
