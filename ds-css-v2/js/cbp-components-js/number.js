fdc420ab39645b84ef8b224e457215aa2abb26cc
Danny Kim <dannyk3941@gmail.com>
2022-06-15 17:17:30 -0400

    chore: change margin heading for demo components

--- ds-css-v2/js/cbp-components-js/number.js
+++ ds-css-v2/js/cbp-components-js/number.js
@@ -1,49 +1,47 @@
-const demoInput = document.getElementById('number-counter');
-const numInput = document.getElementById('numeric-counter');
-const minus = demoInput.querySelectorAll('button')[0];
-const plus = demoInput.querySelectorAll('button')[1];
+const demoInput = document.getElementById("number-counter");
+const numInput = document.getElementById("numeric-counter");
+const minus = demoInput.querySelectorAll("button")[0];
+const plus = demoInput.querySelectorAll("button")[1];
 const textarea = document.querySelector("textarea");
-const textInput = document.getElementById('firstName');
+const textInput = document.getElementById("firstName");
 
-let charCount = document.getElementById('char-count');
-let charCount2 = document.getElementById('char-count-2');
+let charCount = document.getElementById("char-count");
+let charCount2 = document.getElementById("char-count-2");
 let value = numInput;
 
-minus.addEventListener('click', () => {
-  numInput.value--;
-})
+class NumberInput {
+  constructor(id) {
+    this.numCounterNode = document.getElementById(id)
+    this.input = this.numCounterNode.querySelector('input[type="number"]');
+    this.value = this.input.value;
+    this.minus = this.numCounterNode.querySelector('#decrement');
+    this.plus = this.numCounterNode.querySelector('#increment');
 
-plus.addEventListener('click', () => {
-  numInput.value++;
-})
+    this.plus.addEventListener('click', this.increment(10), false)
+    console.log(this.plus);
+  }
 
-numInput.addEventListener('change', (e) => {
-  console.log(e.target.value);
-})
-
-
-textarea.addEventListener("input", event => {
-    const target = event.currentTarget;
-    const maxLength = target.getAttribute("maxlength");
-    const currentLength = target.value.length;
-
-    if (currentLength >= maxLength) {
-        return console.log("You have reached the maximum number of characters.");
+  increment(step = 1) {
+    console.log('increment was called');
+    if (step > 1) {
+      this.value += step;
+    } else {
+      this.value++;
     }
-    
-    charCount.innerHTML = maxLength - currentLength;
-    // console.log(`${maxLength - currentLength} chars left`);
-});
+  }
+}
 
-textInput.addEventListener("input", event => {
-  const target = event.currentTarget;
+const demoNumInput = new NumberInput('number-counter');
+
+const countChar = (id, event) => {
+  const { currentTarget: target } = event;
   const maxLength = target.getAttribute("maxlength");
   const currentLength = target.value.length;
+  const count = document.getElementById(id);
 
   if (currentLength >= maxLength) {
-      return console.log("You have reached the maximum number of characters.");
+    return console.log("You have reached the maximum number of characters.");
   }
-  
-  charCount2.innerHTML = maxLength - currentLength;
-  // console.log(`${maxLength - currentLength} chars left`);
-});
+
+  count.innerHTML = maxLength - currentLength;
+};
