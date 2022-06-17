2002925ccad9a06c0609081374cf1d84e02c811c
Danny Kim <dannyk3941@gmail.com>
2022-06-16 22:00:45 -0400

    fix: refactor toggle js

--- ds-css-v2/js/cbp-components-js/toggle.js
+++ ds-css-v2/js/cbp-components-js/toggle.js
@@ -1,15 +1,57 @@
-const getCheck = document.getElementById('demo-toggle');
+class Toggle {
+  constructor(node) {
+    this.node = node; // Toggle wrapper
+    this.checkbox = node.querySelector('input[type="checkbox"]'); // Toggle checkbox
+    this.slider = this.checkbox.nextElementSibling; // Slider (span) element should always be adjacent to checkbox input
+    this.firstIcon = this.slider.firstElementChild;
+    this.lastIcon = this.slider.lastElementChild;
 
-const input = getCheck.querySelector('input');
-const first = getCheck.querySelector('.slider').firstElementChild;
-const last = getCheck.querySelector('.slider').lastElementChild;
+    this.node.addEventListener("change", (e) => {
+      this.toggleIcons(e);
+    });
+  }
 
-input.onchange = (e) => {
-  if (e.target.checked) {
-    first.style.display = "inline-block";
-    last.style.display = "none";
-  } else {
-    first.style.display = "none";
-    last.style.display = "inline-block";
+  isChecked(event = null) {
+    if (event) {
+      return event.target.checked;
+    } else {
+      return this.checkbox.checked;
+    }
+  }
+
+  isCheckbox(event) {
+    return event.target.type === "checkbox";
+  }
+
+  containsIcons() {
+    return this.slider.children.length > 0;
+  }
+
+  toggleIcons(event) {
+    if (!this.containsIcons()) {
+      return;
+    } else {
+      if (this.isChecked(event)) {
+        this.firstIcon.style.display = "inline-block";
+        this.lastIcon.style.display = "none";
+      } else {
+        this.firstIcon.style.display = "none";
+        this.lastIcon.style.display = "inline-block";
+      }
+    }
   }
 }
+
+class SelectorEngine2 {
+  static getNodes(selector) {
+    return document.querySelectorAll(selector);
+  }
+}
+
+window.addEventListener("load", () => {
+  const allToggles = SelectorEngine2.getNodes('[data-component="cbp-toggle"]');
+
+  allToggles.forEach((toggle) => {
+    new Toggle(toggle);
+  });
+});
