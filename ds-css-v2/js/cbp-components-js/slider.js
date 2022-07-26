4eb8928acec361278c8769c64dd24c537e04608d
Danny Kim <dannyk3941@gmail.com>
2022-07-26 17:43:16 -0400

    chore: cleaning up code in slider.js

--- ds-css-v2/js/cbp-components-js/slider.js
+++ ds-css-v2/js/cbp-components-js/slider.js
@@ -1,7 +1,4 @@
-
-const fillHex = "#745fe9";
-const trackHex = "#afaea2";
-
+// Remove once Selector class is added.
 const allSliders = document.querySelectorAll("[data-slider]");
 
 class Slider {
@@ -12,10 +9,13 @@ class Slider {
     this.valueInput = this.wrapper.querySelector(".cbp-form__number")?.firstElementChild;
     this.min = this.slider.min;
     this.max = this.slider.max;
+    this.fillHex = "#745fe9";
+    this.trackHex = "#afaea2";
     this.styleTag = document.createElement("style");
     this.isChrome = navigator.userAgent.indexOf("Chrome") != -1;
+    this.isSafari = navigator.userAgent.indexOf("Safari") != -1;
 
-    if (this.isChrome) {
+    if (this.isChrome || this.isSafari) {
       this.chromeFill();
     }
 
@@ -23,12 +23,12 @@ class Slider {
   }
 
   handleChange(e, slider) {
-    const gradient = `background: linear-gradient(to right, ${fillHex} 0%, ${fillHex} ${slider.value}%, ${trackHex} ${slider.value}%, ${trackHex} 100%)`;
+    const gradient = `background: linear-gradient(to right, ${this.fillHex} 0%, ${this.fillHex} ${slider.value}%, ${this.trackHex} ${slider.value}%, ${this.trackHex} 100%)`;
     const property = `#${this.slider.id}::-webkit-slider-runnable-track {${gradient}}`;
 
     this.styleTag.innerHTML = property;
 
-    if (this.type == 'value') {
+    if (this.type == "value") {
       this.valueInput.value = slider.value;
     }
   }
@@ -36,31 +36,31 @@ class Slider {
   handleInput(e, slider) {
     this.slider.value = this.valueInput.value;
 
-    const gradient = `background: linear-gradient(to right, ${fillHex} 0%, ${fillHex} ${slider.value}%, ${trackHex} ${slider.value}%, ${trackHex} 100%)`;
+    const gradient = `background: linear-gradient(to right, ${this.fillHex} 0%, ${this.fillHex} ${slider.value}%, ${this.trackHex} ${slider.value}%, ${this.trackHex} 100%)`;
     const property = `#${slider.id}::-webkit-slider-runnable-track {${gradient}}`;
 
     this.styleTag.innerHTML = property;
   }
 
   chromeFill() {
-    const gradient = `background: linear-gradient(to right, ${fillHex} 0%, ${fillHex} ${this.slider.value}%, ${trackHex} ${this.slider.value}%, ${trackHex} 100%)`;
+    const gradient = `background: linear-gradient(to right, ${this.fillHex} 0%, ${this.fillHex} ${this.slider.value}%, ${this.trackHex} ${this.slider.value}%, ${this.trackHex} 100%)`;
     const property = `#${this.slider.id}::-webkit-slider-runnable-track {${gradient}}`;
 
-    this.styleTag.innerHTML =property;
+    this.styleTag.innerHTML = property;
 
     document.head.appendChild(this.styleTag);
   }
 
-  addListeners (slider) {
+  addListeners(slider) {
     slider.addEventListener("input", (e) => {
       this.handleChange(e, slider);
     });
 
-    if (this.type == 'value') {
+    if (this.type == "value") {
       this.valueInput.addEventListener("change", (e) => {
         this.handleInput(e, slider);
       });
-  
+
       this.valueInput.addEventListener("keypress", (e) => {
         if (e.key === "Enter") {
           e.preventDefault();
