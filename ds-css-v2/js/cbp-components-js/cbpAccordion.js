bf0ae3ea0606d59251b26a18c92bfefeb30c54a9
Danny Kim <dannyk3941@gmail.com>
2022-09-08 11:17:30 -0400

    chore: code cleanup css of accordion

--- ds-css-v2/js/cbp-components-js/cbpAccordion.js
+++ ds-css-v2/js/cbp-components-js/cbpAccordion.js
@@ -1,18 +1,29 @@
-const accordionBtn = document.querySelectorAll('.cbp-accordion__title');
+const accordionBtn = document.querySelectorAll(".cbp-accordion__title");
 
 class Accordion {
   constructor(domNode) {
-    this.accordionNode = domNode;
+    this.button = domNode;
+    this.item = this.button.closest(".cbp-accordion__item");
+    this.content = this.item.querySelector(".cbp-accordion__content");
+    this.ariaExpanded = this.button.getAttribute("aria-expanded");
+    this.ariaControls = this.button.getAttribute("aria-controls");
 
-    this.accordionNode.addEventListener('click', this.toggle)
+    this.button.addEventListener("click", (e) => {
+      this.toggle(e, this.item);
+    });
   }
 
-  toggle(event) {
-    const wrapper = event.target.closest('.cbp-accordion__item');
-    wrapper.classList.toggle('active');
+  toggle(event, item) {
+    item.classList.toggle("active");
+
+    if (item.classList.contains("active")) {
+      this.button.setAttribute("aria-expanded", true);
+    } else {
+      this.button.setAttribute("aria-expanded", false);
+    }
   }
 }
 
-accordionBtn.forEach(btn => {
-  new Accordion(btn);
-})
+accordionBtn.forEach((btn) => {
+  return new Accordion(btn);
+});
